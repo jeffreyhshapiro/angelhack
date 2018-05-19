f1a93c8b2fa8b83f76c85210328e98d15096497c
Jeff Shapiro <jeffreyhshapiro@gmail.com>
2018-05-19 14:50:03 -0400

    scoping issues

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,11 +4,42 @@ import MessageWindow from './MessageWindow.js';
 import '../styles/ChatWindow.css'
 
 class ChatWindow extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            messages: ["Hello World", "How are you doing today?", "Lorem ipsum dolor sit amet", "Let's write some code"],
+        }
+    }
+
+    createMessage(e) {
+        e.preventDefault();
+
+        console.log(this.state.userMessage)
+
+        return;
+        this.setState({
+            messages: this.state.messages.push(this.state.userMessage)
+        })
+    }
+
+    userMessage(e) {
+
+        const message = e.target.value;
+
+        console.log(e.target.value)
+
+        this.setState({
+            userMessage: message
+        });
+    }
+
+
     render() {
         return (
             <div className="ChatWindow">
-                <MessageWindow />
-                <InputField />
+                <MessageWindow messages={this.state.messages} />
+                <InputField createMessage={this.createMessage} setMessage={this.userMessage} />
             </div>
         );
     }
